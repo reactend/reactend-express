63c33ce13c388cdc3aae187b9753bb375824ab71
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2021-02-08 14:52:05 +0400

    updated examples

--- examples/advanced/index.js
+++ examples/advanced/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactExpress from "../../src/renderer";
+import { ReactXpress, App, Static, Router, Get } from "../../lib";
 import { postHandler, postsHandler } from "./handlers";
 
 const ExpressApp = () => (
-  <app port={8080}>
-    <static publicPath="/public" />
-    <router path="/">
-      <get path="/posts" handler={postsHandler} />
-      <get path="/posts/:id" handler={postHandler} />
-      <get path="*" content="Not Found" status={404} />
-    </router>
-  </app>
+  <App port={8080}>
+    <Static publicPath="/public" />
+    <Router path="/">
+      <Get path="/posts" handler={postsHandler} />
+      <Get path="/posts/:id" handler={postHandler} />
+      <Get path="*" content="Not Found" status={404} />
+    </Router>
+  </App>
 );
 
-ReactExpress.render(<ExpressApp />);
+ReactXpress.render(<ExpressApp />);
